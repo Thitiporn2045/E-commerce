9f5854c646ad872cc336c231b1308eee29ba0ec2
FameNu <110712106+FameNu@users.noreply.github.com>
2025-03-21 13:59:16 +0700

    add test for testcase 09

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from '@playwright/test';
 
+const BASE_URL = 'http://localhost:3000'
+
+const toastMsg = (prodcutName: string): string =>
+  `Added ${prodcutName} to`
+
+const canColdProductName = 'กระป๋องเก็บความเย็น'
+const poloProductName = 'เสื้อโปโล'
+
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000/'); // ตรวจสอบให้แน่ใจว่า URL ถูกต้อง
     await page.evaluate(() => {
@@ -33,3 +41,28 @@ test('TC04', async ({ page }) => {
     await page.getByRole('link', { name: 'Cart' }).click();
     await page.getByRole('heading', { name: 'Your Collection' }).click();
 });
+
+test.describe(`Add ${canColdProductName} and ${poloProductName} to cart success`, () => {
+  test(`Add ${canColdProductName} 99 to cart and ${poloProductName} 50`, async ({ page }) => {
+    await page.goto(BASE_URL)
+    await page.locator('#quantity-1').click()
+    await page.locator('#quantity-1').fill('99')
+    await expect(page.locator('#quantity-1')).toHaveValue('99')
+    await page.locator('.w-full').first().click() // click add to cart
+
+    await expect(page.getByText(toastMsg(canColdProductName))).toBeVisible()
+
+    const n = 50
+    for (let i = 0; i < n - 1; i++) {
+      await page
+        .locator(
+          'div:nth-child(2) > .p-6 > .mt-6 > div > div > button:nth-child(3)'
+        )
+        .click()
+    }
+    await expect(page.locator('#quantity-2')).toHaveValue(`${n}`)
+    await page.locator('div:nth-child(2) > .p-6 > .mt-6 > .w-full').click() // click add to cart
+    await expect(page.getByText(toastMsg(poloProductName))).toBeVisible()
+  })
+})
+
